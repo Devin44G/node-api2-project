b08d7d72a501793865f841c9bbdece9fa972f987
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-03-10 17:15:00 -0500

    Created PUT and DELETE endpoints for posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -3,8 +3,6 @@ const Posts = require('../data/db.js');
 
 const router = express.Router();
 
-module.exports = router;
-
 /******** POSTS ENDPOINTS ********/
 router.get('/', (req, res) => {
   // res.status(200).json({post: "here we are"});
@@ -32,3 +30,75 @@ router.post('/', (req, res) => {
 });
 
 /******** POSTS ENDPOINT BY ID ********/
+router.get('/:id', (req, res) => {
+  Posts.findById(req.params.id)
+    .then(post => {
+      if(post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ error: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The post information could not be retrieved." });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Posts.remove(req.params.id)
+    .then(post => {
+      if(!post) {
+        res.status(404).json({ error: "The post with the specified ID does not exist." });
+      } else {
+        res.status(200).json({ message: "Post was deleted" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The post could not be removed" });
+    })
+});
+
+router.put('/:id', (req, res) => {
+  Posts.update(req.params.id, req.body)
+    .then(post => {
+      if(post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ error: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Post information could not be retrieved . . ." });
+    });
+});
+
+/******** COMMENTS ENDPOINTS ********/
+router.get('/:id/comments', (req, res) => {
+  Posts.findPostComments(req.params.id)
+    .then(post => {
+      if(post.comments) {
+        res.status(200).json(post.comments);
+      } else {
+        res.status(404).json({ error: "The post with the specified ID does not exist." })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Error receiving comments data" })
+    });
+});
+
+// router.post('/:id', (req, res) => {
+//   Post.findById(req.params.id)
+//     .then(post => {
+//       res.status(201).json(post);
+//     })
+//     .catch(err => {
+//       if(!req.params.id) {
+//         res.status(404).json({ error: "The post with the specified ID does not exist." });
+//       } else {
+//         res.status(500).json({ error: "The post information could not be retrieved." });
+//       }
+//     });
+// });
+
+module.exports = router;
