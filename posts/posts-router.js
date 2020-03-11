897ac708a9dd2ce3ff1bf2b344b05ee8b67db8cc
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-03-11 10:33:57 -0500

    Completed endpoints for comments

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -76,7 +76,7 @@ router.put('/:id', (req, res) => {
 router.get('/:id/comments', (req, res) => {
   Posts.findPostComments(req.params.id)
     .then(post => {
-      if(post.comments) {
+      if(post) {
         res.status(200).json(post.comments);
       } else {
         res.status(404).json({ error: "The post with the specified ID does not exist." })
@@ -87,18 +87,33 @@ router.get('/:id/comments', (req, res) => {
     });
 });
 
-// router.post('/:id', (req, res) => {
-//   Post.findById(req.params.id)
-//     .then(post => {
-//       res.status(201).json(post);
-//     })
-//     .catch(err => {
-//       if(!req.params.id) {
-//         res.status(404).json({ error: "The post with the specified ID does not exist." });
-//       } else {
-//         res.status(500).json({ error: "The post information could not be retrieved." });
-//       }
-//     });
-// });
+router.post("/:id/comments", (req, res) => {
+	if (req.body.text.length === 0) {
+		res.status(400).json({ errorMessage: "Please provide text for the comment." });
+	} else {
+		Posts.insertComment(req.body)
+		  .then(comment => {
+				if (comment) {
+					Posts.findCommentById(comment.id)
+						.then(comment => {
+							res.status(201).json(comment);
+						})
+						.catch(error => {
+							res.status(500).json({ error: "error retrieving data" });
+						});
+				} else {
+					res.status(404).json({
+							message: "The post with the specified ID does not exist."
+						});
+				}
+			})
+			.catch(error => {
+				console.log(error);
+				res.status(500).json({
+					error: "There was an error while saving the comment to the database"
+				});
+			});
+	}
+});
 
 module.exports = router;
